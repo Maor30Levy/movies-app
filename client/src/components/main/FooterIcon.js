21b244cab655bf74695fbd3c7c5809a2489dae43
Maor30Levy <maor30levy@gmail.com>
2021-06-30 12:45:26 +0300

    fixed footer buttons

--- client/src/components/main/FooterIcon.js
+++ client/src/components/main/FooterIcon.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function FooterIcon({ name }) {
-    const nameLower = name.toLowerCase();
+    const nameLower = name.toLowerCase().trim();
     return (
         <NavLink to={`/${nameLower}`} className="nav" activeClassName="nav_active">
-            <img className="icon" src={`./icons/footer/‏‏${nameLower}_icon.png`} alt="icon" />
-            <img className="icon__active" src={`./icons/footer/‏‏${nameLower}_icon__active.png`} alt="icon" />
+            <img className="icon" src={`./icons/footer/‏‏${name}.png`} alt="icon" />
+            <img className="icon__active" src={`./icons/footer/‏‏${name}__active.png`} alt="icon" />
             <div>{name}</div>
         </NavLink>
     )
